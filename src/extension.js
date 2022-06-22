cc8bd1b279ee898b7b3e76552e888dfc4085bc2d
Dimitar Rusev <mitikodev@gmail.com>
2022-06-22 16:48:34 +0300

    Remove permissions in manifest and randomize signoffs from a list

--- src/extension.js
+++ src/extension.js
@@ -8,7 +8,16 @@ const loaderId = setInterval(() => {
 
     clearInterval(loaderId);
     startExtension(window._gmailjs);
-}, 100);
+}, 200);
+
+window.genzMail = {};
+window.genzMail.signoffs = [
+    "<br>Yikes,<br>&lt;name&gt;",
+    "<br>Slay,<br>&lt;name&gt;",
+    "<br>Consider yourselves warned!<br>&lt;name&gt;",
+    "<br>Disrespectfully,<br>&lt;name&gt;",
+    "<br>Disrespectfully,<br>&lt;name&gt;",
+];
 
 function startExtension(gmail) {
     window.gmail = gmail;
@@ -17,14 +26,34 @@ function startExtension(gmail) {
     gmail.observe.on("load", () => {
         console.log("Gmail loaded.");
 
-        gmail.observe.on('compose', function(compose, _) {
-            gmail.tools.add_compose_button(compose, 'Yassify', () => {
-                console.log("Yassified! Slay!");
-                let mail_body = compose.body();
-                let yassified_signoff = "<br>Yikes,<br>&lt;name&gt;"
-                compose.body(mail_body + yassified_signoff);
-            }, 'genz-mail-yassified');
+        gmail.observe.on('compose', function(compose_ref, _) {
+            // @param {gmailjs object} compose_ref - The gmailjs compose object
+            // @param {string} content_html - Content of the button
+            // @param {function} onclick_action - on_click event handler
+            // @param {string} custom_style_class - a class only added to the button
+            gmail.tools.add_compose_button(compose_ref, 'Yassify', () => {
+                console.log("Yassified! Slay! New"); // for debug purposes
+                // cache the email so far
+                let mail_body = compose_ref.body();
+                // TODO: try to find a previously inserted signoff and:
+                // case 1: if found, replace with new one
+                // case 2: if not found, insert new random one
+
+                // TODO: maybe add a list page for possible signoffs
+                // - you could select all options
+                // - you could disselect all options -> default is there - just <name>
+                // - you could toggle specific options
+
+                // choose a random signoff
+                let yassified_signoff = chooseRandomSignoff();
+                compose_ref.body(mail_body + yassified_signoff);
+            }, 'genz-mail-btn');
         });
     });
 
+}
+
+function chooseRandomSignoff() {
+    const randomIndex = Math.floor(Math.random() * window.genzMail.signoffs.length);
+    return window.genzMail.signoffs[randomIndex];
 }
